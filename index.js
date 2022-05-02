b3b96bc3a3e83317e0ae7308bdc173935d782344
Muthun0916 <76974949+Muthun0916@users.noreply.github.com>
2022-05-02 23:43:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,360 @@
-/* Requiring all the needed stuff for the Command-Handler and client to work. */
+const { entersState,joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+const yts = require('yt-search');
+const eventEmitter = require('events');
+const activeSongs = new Map();
+const event = new eventEmitter();
 
-const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
-const music = require('@koenie06/discord.js-music');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
-module.exports.client = client;
+module.exports.event = event;
 
-/* This will run when a new song started to play */
-music.event.on('playSong', (channel, songInfo, requester) => {
-	channel.send({ content: `Started playing the song [${songInfo.title}](${songInfo.url}) - ${songInfo.duration} | Requested by \`${requester.tag}\`` });
-});
+exports.play = async (options = {}) => {
 
-/* This will run when a new song has been added to the queue */
-music.event.on('addSong', (channel, songInfo, requester) => {
-	channel.send({ content: `Added the song [${songInfo.title}](${songInfo.url}) - ${songInfo.duration} to the queue | Added by \`${requester.tag}\`` });
-});
+    const { interaction, channel, song , show} = options;
+    if(!channel || channel?.type !== 'GUILD_VOICE') throw new Error(`INVALID_VOICE_CHANNEL: There is no valid VoiceChannel provided.`);
+    if(!song || typeof song !== 'string') throw new Error(`INVALID_MUSIC_URL: There is no valid Music URL provided.`);
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`)
 
-/* This will run when a song started playing from a playlist */
-music.event.on('playList', async (channel, playlist, songInfo, requester) => {
-    channel.send({
-        content: `Started playing the song [${songInfo.title}](${songInfo.url}) by \`${songInfo.author}\` of the playlist ${playlist.title}.
-        This was requested by ${requester.tag} (${requester.id})`
-    });
-});
+    const data = activeSongs.get(channel.guild.id) || {};
 
-/* This will run when a new playlist has been added to the queue */
-music.event.on('addList', async (channel, playlist, requester) => {
-    channel.send({
-        content: `Added the playlist [${playlist.title}](${playlist.url}) with ${playlist.videos.length} amount of videos to the queue.
-        Added by ${requester.tag} (${requester.id})`
-    });
-});
+    if(!channel.guild.me.voice.channel) {
+        data.connection = await connectToChannel(channel);
+    };
+    if(!data.connection) {
+        data.connection = await connectToChannel(channel);
+    };
 
-/* This will run when all the music has been played, and the bot disconnects. */
-music.event.on('finish', (channel) => {
-	channel.send({ content: `All music has been played, disconnecting..` });
-});
+    if(!data.queue) data.queue = [];
+    if(!data.repeat) data.repeat = false;
 
-/* Setting every command in the 'commands' folder into the client.commands Collection. */
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    data.guildId = channel.guild.id;
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+    let queueSongInfo;
 
-	client.commands.set(command.data.name, command);
+
+        const ytdlSongInfo = await ytdl.getInfo(song);
+        console.log(ytdlSongInfo["videoDetails"]);
+
+        queueSongInfo = {
+            title: ytdlSongInfo["videoDetails"]["title"],
+            author: ytdlSongInfo["videoDetails"]["author"],
+            duration: ytdlSongInfo["videoDetails"]["lengthSeconds"],
+            url: song,
+            thumbnail: ytdlSongInfo["videoDetails"]["thumbnails"].slice(-1)[0]["url"],
+            extra: {
+                type: 'video',
+                playlist: null
+            }
+        };
+
+        await data.queue.push({
+            info: queueSongInfo,
+            requester: interaction.user,
+            url: song,
+            channel: interaction.channel
+        });
+
+    if(!data.dispatcher) {
+
+        playSong(data, interaction);
+        if(show==="no")await interaction.channel.send(`これ再生するよ ${song} `);
+
+    } else {
+
+        if(queueSongInfo.extra.type === 'playlist') {
+            event.emit('addList', interaction.channel, queueSongInfo.extra.playlist, interaction.user);
+        } else {
+            event.emit('addSong', interaction.channel, queueSongInfo, interaction.user);
+        }
+
+    };
+
+    activeSongs.set(channel.guild.id, data);
+
+};
+
+exports.isConnected = async (options = {}) => {
+
+    const { interaction } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`)
+
+    const fetchedData = activeSongs.get(interaction.guild.id);
+
+    if(!fetchedData?.connection && !fetchedData?.player) return Boolean(false)
+    else return Boolean(true)
+
+};
+
+exports.stop = async (options = {}) => {
+
+    const { interaction } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`)
+
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");
+
+    const fetchedData = await activeSongs.get(interaction.guild.id);
+
+    fetchedData.player.stop();
+    fetchedData.connection.destroy();
+    activeSongs.delete(interaction.guild.id);
+
+};
+
+exports.repeat = async(options = {}) => {
+
+    const { interaction, value } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
+    if(!value) value === false;
+    if(value === undefined || typeof value !== 'boolean') throw new Error(`INVALID_BOOLEAN: There is no valid Boolean provided.`);
+
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");;
+
+    const fetchedData = await activeSongs.get(interaction.guild.id);
+
+    if(fetchedData?.repeat === value) return interaction.channel.send("すでにリピートするようになってるよ");
+
+    fetchedData.repeat = value;
+    activeSongs.set(interaction.guild.id, fetchedData);
+
 }
 
-/* This code will run when the client has been logged in. */
+exports.isRepeated = async (options = {}) => {
 
-client.on('ready', async() => {
-	console.log('I am online!');
-});
+    const { interaction } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`)
 
-/* This code will run when the client has received a Interaction. */
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player)return interaction.channel.send("再生する音楽がないよ");
 
-client.on('interactionCreate', async interaction => {
+    const fetchedData = activeSongs.get(interaction.guild.id);
 
-    /* If it isn't a command, return. */
-	if (!interaction.isCommand()) return;
+    return Boolean(fetchedData.repeat);
 
-    /* Getting all the setted client's commands that has been set in deploy-commands.js. */
-	const command = client.commands.get(interaction.commandName);
+}
 
-    /* If there are no commands, return. */
-	if (!command) return;
+exports.skip = async(options = {}) => {
 
-    /* Try executing the command. If this doesn't work, throw a error. */
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	};
-});
+    const { interaction } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`)
 
-/* Logging the client in */
-client.login(token);
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player)return interaction.channel.send("再生する音楽がないよ");
+
+    const fetchedData = await activeSongs.get(interaction.guild.id);
+    const player = await fetchedData.player;
+    const connection = await fetchedData.connection
+
+    const finishChannel = await fetchedData.queue[0].channel
+    await fetchedData.queue.shift();
+
+    if(fetchedData.queue.length > 0) {
+
+        activeSongs.set(interaction.guild.id, fetchedData);
+
+        playSong(fetchedData, interaction)
+
+    } else {
+
+        await event.emit('finish', finishChannel);
+        await activeSongs.delete(interaction.guild.id);
+
+        await player.stop();
+        await connection.destroy();
+
+    };
+
+};
+
+exports.pause = async (options = {}) => {
+
+    const { interaction } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`)
+
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");
+
+    const fetchedData = activeSongs.get(interaction.guild.id);
+    const player = fetchedData.player;
+
+    if(player.state.status === 'paused') return interaction.channel.send("すでに停止しているよ");
+
+    player.pause();
+
+}
+
+exports.isPaused = async (options = {}) => {
+
+    const { interaction } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`)
+
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");
+
+    const fetchedData = activeSongs.get(interaction.guild.id);
+    const player = fetchedData.player;
+
+    if(player.state.status === 'paused') return Boolean(true)
+    else return Boolean(false)
+
+}
+
+exports.resume = async (options = {}) => {
+
+    const { interaction } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`)
+
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");
+    const fetchedData = activeSongs.get(interaction.guild.id);
+    const player = fetchedData.player;
+
+    if(player.state.status === 'playing') return interaction.channel.send("すでに再生されてるよ");
+
+    player.unpause();
+
+}
+
+exports.isResumed = async (options = {}) => {
+
+    const { interaction } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`)
+
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");
+
+    const fetchedData = activeSongs.get(interaction.guild.id);
+    const player = fetchedData.player;
+
+    if(player.state.status === 'playing') return Boolean(true)
+    else return Boolean(false)
+
+}
+
+exports.jump = async (options = {}) => {
+
+    const { interaction, number } = options
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
+    if(!number || !Number.isInteger(number)) return interaction.channel.send("数字を入力してね");
+
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");
+
+    const fetchedData = await activeSongs.get(interaction.guild.id);
+
+    if(number > fetchedData.queue.length) return interaction.channel.send("もうすこし小さな数字を入力してね");
+
+    await fetchedData.queue.splice(0, number);
+    activeSongs.set(interaction.guild.id, fetchedData);
+
+    playSong(activeSongs.get(interaction.guild.id), interaction);
+
+}
+
+exports.getQueue = async (options = {}) => {
+
+    const { interaction } = options
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");
+
+    const fetchedData = await activeSongs.get(interaction.guild.id);
+
+    return (fetchedData.queue);
+
+};
+
+exports.removeQueue = async (options = {}) => {
+
+    const { interaction, number } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
+    if(!number || !Number.isInteger(number)) return interaction.channel.send("数字を入力してね");
+
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");
+
+    const fetchedData = await activeSongs.get(interaction.guild.id);
+    if(fetchedData.queue.length < number) return interaction.channel.send("もうすこし小さな数字を入力してね");
+
+    const spliceNumber = number - 1;
+    fetchedData.queue.splice(spliceNumber,1);
+
+};
+
+exports.volume = async (options = {}) => {
+
+    const { interaction, volume } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
+    if(!volume || !Number.isInteger(volume) || volume > 100) throw new Error(`INVALID_VOLUME: There is no valid Volume Integer provided or the number is higher than 100.`);
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) return interaction.channel.send("再生する音楽がないよ");
+
+    const fetchedData = await activeSongs.get(interaction.guild.id);
+
+    fetchedData.resource.volume.setVolume(volume)
+
+};
+
+async function playSong(data, interaction) {
+
+    let resource = await createAudioResource(ytdl(data.queue[0].url, { filter: 'audioonly' ,highWaterMark: 32 * 1024 * 1024,}), {
+        inputType: StreamType.Arbitrary,
+        inlineVolume: true,
+    });
+
+    const player = createAudioPlayer();
+
+    player.play(resource);
+
+    data.player = player;
+    data.resource = resource
+    data.dispatcher = await data.connection.subscribe(player);
+    data.dispatcher.guildId = data.guildId;
+
+    if(data.queue[0].info.extra.type === 'playlist') {
+        event.emit('playList', data.queue[0].channel, data.queue[0].info.extra.playlist, data.queue[0].info, data.queue[0].requester);
+    } else {
+        event.emit('playSong', data.queue[0].channel, data.queue[0].info, data.queue[0].requester);
+    }
+
+    await entersState(player,AudioPlayerStatus.Playing, 10 * 1000);
+     await entersState(player,AudioPlayerStatus.Idle, 24 * 60 * 60 * 1000);
+     finishedSong(player, data.connection, data.dispatcher, interaction);
+
+    player.on('error', err => console.log(err))
+
+};
+
+async function finishedSong(player, connection, dispatcher, interaction) {
+
+    const fetchedData = await activeSongs.get(dispatcher.guildId);
+
+    if(fetchedData?.repeat === true) return playSong(fetchedData, interaction)
+
+    await fetchedData.queue.shift();
+
+    if(fetchedData.queue.length > 0) {
+
+        activeSongs.set(dispatcher.guildId, fetchedData);
+
+        playSong(fetchedData, interaction)
+
+    } else {
+
+        event.emit('finish', interaction.channel);
+
+        activeSongs.delete(dispatcher.guildId);
+
+        player.stop();
+        connection.destroy();
+
+    };
+
+};
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+async function connectToChannel(channel) {
+    return new Promise(async(resolve, reject) => {
+        const connection = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+            selfDeaf: false
+        });
+        connection.once(VoiceConnectionStatus.Ready, () => {
+            resolve(connection)
+        })
+        await delay(30000)
+        reject('Connection was failed to connect to VC')
+    })
+}
